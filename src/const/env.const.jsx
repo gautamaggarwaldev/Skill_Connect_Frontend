f7de5bc4d0456f8207e765748eef04308791407d
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-03-12 12:03:39 +0530

    setup constant variables, token helper and user Store

---
+++ src/const/env.const.jsx
@@ -0,0 +1,4 @@
+const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:7000";
+const RAZORPAY_KEY_ID = import.meta.env.REACT_APP_RAZORPAY_KEY_ID;
+
+export { BASE_URL, RAZORPAY_KEY_ID };
